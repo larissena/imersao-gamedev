105eb6098bf2df73cf34ba2e9abe6da2e281f3f1
Larissa Sena <larissa.sena@mail.com>
2020-06-30 18:22:15 -0300

    Insere pulo

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,8 +1,8 @@
 class Inimigo extends Animacao {
-    constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-        super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite);
+    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+        super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
 
-        this.velocidade = 10;
+        this.velocidade = 8;
     }
 
     move() {
