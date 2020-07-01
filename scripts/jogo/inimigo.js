d4b9b9c2055f2c1117cd78fc66cb1f432e386cfb
Larissa Sena <larissa.sena@mail.com>
2020-07-01 15:35:41 -0300

    Insere máximo de pulos

--- scripts/jogo/inimigo.js
+++ scripts/jogo/inimigo.js
@@ -1,15 +1,15 @@
 class Inimigo extends Animacao {
-    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
-        super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
 
-        this.velocidade = 8;
+    this.velocidade = 8;
+  }
+
+  move() {
+    this.x -= this.velocidade;
+
+    if (this.x < -this.largura) {
+      this.x = width;
     }
-
-    move() {
-        this.x -= this.velocidade;
-
-        if (this.x < -this.largura) {
-            this.x = width;
-        }
-    }
+  }
 }
--- scripts/jogo/personagem.js
+++ scripts/jogo/personagem.js
@@ -1,41 +1,48 @@
 class Personagem extends Animacao {
-    constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
-        super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite);
 
-        this.variacaoY = variacaoY;
-        this.yInicial = height - this.altura - this.variacaoY;
-        this.y = this.yInicial;
+    this.variacaoY = variacaoY;
+    this.yInicial = height - this.altura - this.variacaoY;
+    this.y = this.yInicial;
 
-        this.gravidade = 3;
-        this.velocidadePulo = 0;
+    this.gravidade = 6;
+    this.alturaDoPulo = -50;
+    this.velocidadePulo = 0;
+    this.pulos = 0;
+  }
+
+  pula() {
+    if (this.pulos < 2) {
+      this.velocidadePulo = this.alturaDoPulo;
+      this.pulos++;
     }
 
-    pula() {
-        this.velocidadePulo = -30;
+  }
+
+  aplicaGravidade() {
+    this.y += this.velocidadePulo;
+    this.velocidadePulo += this.gravidade;
+
+    if (this.y > this.yInicial) {
+      this.y = this.yInicial;
+      this.pulos = 0;
     }
+  }
 
-    aplicaGravidade() {
-        this.y += this.velocidadePulo;
-        this.velocidadePulo += this.gravidade;
+  estaColidindo(inimigo) {
+    const precisao = 0.65;
+    const colisao = collideRectRect(
+      this.x,
+      this.y,
+      this.largura * precisao,
+      this.altura * precisao,
+      inimigo.x,
+      inimigo.y,
+      inimigo.largura * precisao,
+      inimigo.altura * precisao
+    );
 
-        if (this.y > this.yInicial) {
-            this.y = this.yInicial;
-        }
-    }
-
-    estaColidindo(inimigo) {
-        const precisao = 0.65;
-        const colisao = collideRectRect(
-            this.x,
-            this.y,
-            this.largura * precisao,
-            this.altura * precisao,
-            inimigo.x,
-            inimigo.y,
-            inimigo.largura * precisao,
-            inimigo.altura * precisao
-        );
-
-        return colisao;
-    }
+    return colisao;
+  }
 }
