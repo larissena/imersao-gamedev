8cb8e4aebf1f5b578612b7f284b62390bc465b8b
Larissa Sena <larissa.sena@mail.com>
2020-07-01 18:35:58 -0300

    Insere tela inicial + botao

---
+++ scripts/load.js
@@ -0,0 +1,15 @@
+function preload() {
+  imagemTelaInicial = loadImage('imagens/cenario/tela-inicial.png');
+  fonteTelaInicial = loadFont('imagens/assets/fonteTelaInicial.otf');
+  imagemCenario = loadImage('imagens/cenario/floresta.png');
+  imagemGameOver = loadImage('imagens/assets/game-over.png');
+
+  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+
+  imagemInimigo = loadImage('imagens/inimigos/gotinha.png');
+  imagemInimigoGrande = loadImage('imagens/inimigos/troll.png');
+  imagemInimigoVoador = loadImage('imagens/inimigos/gotinha-voadora.png');
+
+  somDoJogo = loadSound('sons/trilha_jogo.mp3');
+  somPulo = loadSound('sons/somPulo.mp3');
+}
